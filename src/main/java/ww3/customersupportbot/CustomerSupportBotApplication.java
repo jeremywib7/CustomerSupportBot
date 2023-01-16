d741171940a4e16c96874e93351cc48fd7a1025d
jeremyyonathan <jeremywib7@gmail.com>
2023-01-16 13:51:30 +0700

    Initial commit

---
+++ src/main/java/ww3/customersupportbot/CustomerSupportBotApplication.java
@@ -0,0 +1,13 @@
+package ww3.customersupportbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSupportBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSupportBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ww3/customersupportbot/CustomerSupportBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ww3.customersupportbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSupportBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
